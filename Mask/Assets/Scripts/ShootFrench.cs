66d000f92d50cb3f1c33e0ba02a5c06931b35927
Hadrien Belleville <85734187+vraus@users.noreply.github.com>
2026-01-31 16:43:36 +0100

    [add] Sounds to french soldiers

--- Mask/Assets/Scripts/ShootFrench.cs
+++ Mask/Assets/Scripts/ShootFrench.cs
@@ -5,11 +5,12 @@ public class ShootFrench : MonoBehaviour
     [SerializeField] GameObject target;
     [SerializeField] GameObject Muzzle;
     [SerializeField] float destroyAfter = 2f;
-    [SerializeField] AudioSource shootSound;
+    [SerializeField] AudioClip shootSound;
 
     public void Shoot()
     {
         GameObject instance = Instantiate(target, Muzzle.transform.position, Muzzle.transform.rotation);
+        SoundFXManager.instance.PlayAudioClip(shootSound, transform, 1f);
         if (destroyAfter > 0f)
             Destroy(instance, destroyAfter);
     }
