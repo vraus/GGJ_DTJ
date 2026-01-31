de1eef7641ec4eb71731637a5e42a0d5d7f4ee13
Hadrien Belleville <85734187+vraus@users.noreply.github.com>
2026-01-31 18:31:28 +0100

    [add] subtitle system

---
+++ Mask/Assets/Scripts/CinematicManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class CinematicManager : MonoBehaviour
+{
+    private static CinematicManager _instance;
+
+    public static CinematicManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    [Header("Subtitles")]
+    [SerializeField] private GameObject subtitles;
+    [SerializeField] private TextMeshProUGUI subtitleText;
+    [SerializeField] private AudioObject[] radioScriptAudioClips;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        Say();
+    }
+
+    public void SetSubtitle(string text)
+    {
+        subtitleText.text = text;
+    }
+
+    public void ClearSubtitle()
+    {
+        subtitleText.text = "";
+    }
+
+    public void Say()
+    {
+        StartCoroutine(SayCoroutine());
+    }
+
+    private IEnumerator SayCoroutine()
+    {
+        foreach (AudioObject clip in radioScriptAudioClips)
+        {
+            StartCoroutine(PlayAudioWithSubtitles(clip));
+            yield return new WaitForSeconds(clip.audioClip.length + 0.5f);
+        }
+    }
+
+    private IEnumerator PlayAudioWithSubtitles(AudioObject clip)
+    {
+        audioSource.PlayOneShot(clip.audioClip);
+        SetSubtitle(clip.subtitle);
+        yield return new WaitForSeconds(clip.audioClip.length);
+        ClearSubtitle();
+    }
+
+}
