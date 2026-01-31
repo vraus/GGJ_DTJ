0d7acf104090ccbd5b6d954a61807ed5e752719e
Deryne Four <derynef@gmail.com>
2026-01-31 23:03:52 +0100

    muzzle fixed

--- Mask/Assets/Scripts/CleanMuzzle.cs
+++ Mask/Assets/Scripts/CleanMuzzle.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class CleanMuzzle : MonoBehaviour
 {
-
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
 }
--- Mask/Assets/Scripts/Player/InputManager.cs
+++ Mask/Assets/Scripts/Player/InputManager.cs
@@ -67,7 +67,6 @@ public class InputManager : MonoBehaviour
 
     public bool IsDroppedPressed()
     {
-        Debug.Log("Drop Pressed: " + _playerControls.Locomotion.Drop.triggered);
         return _playerControls.Locomotion.Drop.triggered;
     }
 
