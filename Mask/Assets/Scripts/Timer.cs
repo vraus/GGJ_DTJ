5104b84bafdfbd8882fa458a2733c682b6adcddb
Hadrien Belleville <85734187+vraus@users.noreply.github.com>
2026-02-01 12:10:15 +0100

    [add] Timer begin

---
+++ Mask/Assets/Scripts/Timer.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float timer = 90.0f;
+
+    void Start()
+    {
+
+    }
+}
