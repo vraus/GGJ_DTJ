79cd470f7f193ed9dad7ec55f5e7526c9b9918f3
Deryne Four <derynef@gmail.com>
2026-02-01 10:00:11 +0100

    save volume preferences

--- Mask/Assets/Scripts/Audio/SoundMixerManager.cs
+++ Mask/Assets/Scripts/Audio/SoundMixerManager.cs
@@ -6,18 +6,53 @@ public class SoundMixerManager : MonoBehaviour
 {
     [SerializeField] public AudioMixer audioMixer;
 
+    private static SoundMixerManager instance;
+
+    private void Awake()
+    {
+        // Implement singleton pattern
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // Load saved settings
+        LoadAudioSettings();
+    }
+
     public void SetMasterVolume(float volume)
     {
         audioMixer.SetFloat("masterVolume", volume);
+        PlayerPrefs.SetFloat("masterVolume", volume);
+        PlayerPrefs.Save();
     }
 
     public void SetMusicVolume(float volume)
     {
         audioMixer.SetFloat("musicVolume", volume);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+        PlayerPrefs.Save();
     }
 
     public void SetSFXVolume(float volume)
     {
         audioMixer.SetFloat("sfxVolume", volume);
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadAudioSettings()
+    {
+        float masterVol = PlayerPrefs.GetFloat("masterVolume", 0f);
+        float musicVol = PlayerPrefs.GetFloat("musicVolume", 0f);
+        float sfxVol = PlayerPrefs.GetFloat("sfxVolume", 0f);
+
+        audioMixer.SetFloat("masterVolume", masterVol);
+        audioMixer.SetFloat("musicVolume", musicVol);
+        audioMixer.SetFloat("sfxVolume", sfxVol);
     }
 }
